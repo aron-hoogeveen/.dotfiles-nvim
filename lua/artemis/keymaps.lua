111463586468ab5e2845a019c8aad3c84ff61ce9
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2025-07-05 17:20:50 +0200

    Remap Telescope bindings

--- lua/artemis/keymaps.lua
+++ lua/artemis/keymaps.lua
@@ -10,8 +10,10 @@ local map = vim.keymap.set
 ---    ██    ███████ ███████ ███████ ███████  ██████  ██████  ██      ███████ 
 ---
 ----------------------------------------------------------------------------------------------------
-map('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-map('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+
+map("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "[F]ind [F]iles" })
+map("n", "<leader>fg", require("telescope.builtin").git_files, { desc = "[F]uzzy search [G]it files" })
+map("n", "<leader>fr", require("telescope.builtin").oldfiles, { desc = "[F]ind [R]ecently opened files" })
 map('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -19,23 +21,7 @@ map('n', '<leader>/', function()
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
-
-local function telescope_live_grep_open_files()
-  require('telescope.builtin').live_grep {
-    grep_open_files = true,
-    prompt_title = 'Live Grep in Open Files',
-  }
-end
-map('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
-map('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
-map('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-map('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-map('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-map('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-map('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-map('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
-map('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-map('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+map("n", "<leader>f<space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
 
 
 ----------------------------------------------------------------------------------------------------
