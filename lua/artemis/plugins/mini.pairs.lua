88ff889bad946c17825398766df464ad49f29466
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2025-12-06 13:46:33 +0100

    Disable mini.pairs

--- lua/artemis/plugins/mini.pairs.lua
+++ lua/artemis/plugins/mini.pairs.lua
@@ -1,5 +1,6 @@
 return {
-  'echasnovski/mini.pairs',
-  version = '*',
-  opts = {},
+  -- FIXME disabled since the default config is not nice to use (lot of the time I am deleting extra characters).
+  -- 'echasnovski/mini.pairs',
+  -- version = '*',
+  -- opts = {},
 }
